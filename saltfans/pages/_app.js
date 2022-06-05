a8fde1e1d67e01ee30dc7daead7a566fd4c3ddc0
saltfans <aff.davis@gmail.com>
2022-06-05 23:51:16 +0200

    UPDATE

--- saltfans/pages/_app.js
+++ saltfans/pages/_app.js
@@ -1,11 +1,7 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-require('dotenv').config();
-const mysql = require('mysql2');
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-console.log('Connected to PlanetScale!');
-connection.end();
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
